bdf666345ba2644aeb9d16993853aadf0f11c2dd
Shashank Siripragada <siripragada.s@northeastern.edu>
2022-04-11 19:03:46 -0400

    adding consolidated code to create and drop tables

---
+++ db-create-consolidated.sql
@@ -0,0 +1,33 @@
+use PHMS;
+
+drop table EPOC;
+
+drop table Vaccination;
+
+Drop table PatientDemographics;
+
+Drop table Symptoms;
+Drop table SymptomDetails;
+
+Drop table Diagnosis;
+Drop table DiagnosisDetials;
+
+Drop table LabResults;
+Drop table LabResultDetails;
+
+Drop table VitalSigns;
+Drop table VitalSignDetails;
+
+
+drop table PatientEncounter;
+Drop table Prescription;
+
+
+Drop table HealthcareProvider;
+Drop table MedicationDetails;
+
+
+Drop table Billing;
+Drop table Patient;
+
+drop table InsuranceProvider;
