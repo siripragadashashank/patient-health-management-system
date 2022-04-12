735f6c350890039f66bc530dba4bfb4efffd64f3
Shashank Siripragada <siripragada.s@northeastern.edu>
2022-04-11 20:02:54 -0400

    Update db-create-consolidated.sql

--- db-create-consolidated.sql
+++ db-create-consolidated.sql
@@ -1,33 +1,182 @@
 use PHMS;
 
-drop table EPOC;
+---------------------------DROP--------------------------
 
-drop table Vaccination;
+Drop table if exists  dbo.PatientDemographics;
+drop table if exists dbo.Vaccination;
+drop table if exists dbo.InsuranceProvider;
+drop table if exists dbo.EPOC;
 
-Drop table PatientDemographics;
+Drop table if exists dbo.Diagnosis;
+Drop table if exists dbo.DiagnosisDetails;
 
-Drop table Symptoms;
-Drop table SymptomDetails;
+Drop table if exists dbo.LabResults;
+Drop table if exists dbo.LabResultDetails;
 
-Drop table Diagnosis;
-Drop table DiagnosisDetials;
+Drop table if exists dbo.VitalSigns;
+Drop table if exists dbo.VitalSignDetails;
 
-Drop table LabResults;
-Drop table LabResultDetails;
+Drop table if exists dbo.Symptom;
+Drop table if exists dbo.SymptomDetail;
 
-Drop table VitalSigns;
-Drop table VitalSignDetails;
+Drop table if exists dbo.Billing;
+Drop table if exists dbo.Prescription;
 
+Drop table if exists dbo.MedicationDetails;
+drop table if exists dbo.PatientEncounter;
 
-drop table PatientEncounter;
-Drop table Prescription;
-
-
-Drop table HealthcareProvider;
-Drop table MedicationDetails;
+drop table if exists dbo.Patient;
+Drop table if exists dbo.HealthcareProvider;
 
 
-Drop table Billing;
-Drop table Patient;
+-----------------CREATE----------------------------
 
-drop table InsuranceProvider;
+ CREATE TABLE Patient
+(
+PatID INT PRIMARY KEY, 
+FirstName VARCHAR(45),
+LastName VARCHAR(45),
+DoB DateTime,
+Street VARCHAR(45),
+City VARCHAR(45),
+State VARCHAR(45),
+ZipCode INT,
+PhoneNo INT, 
+EmailAddress VARCHAR(45),
+);
+
+
+CREATE TABLE HealthCareProvider
+(
+HealthCareProviderID INT PRIMARY KEY, 
+Designation VARCHAR(45),
+EmpFirstName VARCHAR(45),
+EmpLastName VARCHAR(45),
+EmpContactNo INT,
+);
+
+ CREATE TABLE PatientEncounter
+(
+PatEncID INT PRIMARY KEY, 
+PatID INT NOT NULL
+REFERENCES Patient(PatID),
+HealthCareProviderID INT NOT NULL
+REFERENCES HealthCareProvider(HealthCareProviderID),
+PatEncAdmitDate DateTime,
+AdmitType VARCHAR(45),
+AdmitLocation VARCHAR(45),
+PatEncDiscDate DATETIME,
+DiscLocation VARCHAR(45),
+);
+
+
+CREATE TABLE SymptomDetail
+(
+SymCode INT PRIMARY KEY, 
+SymName VARCHAR(45)
+);
+
+
+
+CREATE TABLE Symptom
+(
+PatEncID INT NOT NULL
+REFERENCES PatientEncounter(PatEncID),
+SymCode INT NOT NULL
+REFERENCES SymptomDetail(SymCode),
+Duration INT
+);
+
+
+
+
+
+create table DiagnosisDetails
+(
+DxCode int primary key,
+DxName varchar(45)
+);
+
+create table Diagnosis
+(
+PatEncID int not null references PatientEncounter(PatEncID),
+HealthCareProviderID int not null references HealthCareProvider(HealthCareProviderID),
+DxCode int not null references DiagnosisDetails(DxCode)
+);
+
+
+CREATE TABLE LabResultDetails
+(
+TestID INT PRIMARY KEY ,
+TestName VARCHAR(45),
+TestDept VARCHAR(45),
+Price DOUBLE PRECISION
+);
+
+CREATE TABLE LabResults
+(
+PatEncID INT NOT NULL
+REFERENCES PatientEncounter(PatEncID), 
+HealthCareProviderID INT NOT NULL
+REFERENCES HealthCareProvider(HealthCareProviderID),
+TestID INT  NOT NULL REFERENCES LabResultDetails(TestID),
+StoreTime Date,
+Val Double PRECISION,
+ValNum INT
+);
+
+
+CREATE TABLE VitalSignDetails
+(
+VitalID INT PRIMARY KEY ,
+VitalName VARCHAR(45),
+VitalUnit VARCHAR(45)
+);
+
+CREATE TABLE VitalSigns
+(
+PatEncID INT  NOT NULL
+REFERENCES PatientEncounter(PatEncID),
+VitalID INT NOT NULL
+REFERENCES VitalSignDetails(VitalID),
+StoreTime DATETIME,
+VitalVal VARCHAR(45),
+VitalValNum DOUBLE PRECISION
+);
+
+
+CREATE TABLE MedicationDetails
+(
+MedID INT PRIMARY KEY, 
+MedName VARCHAR(45),
+MedPrice DOUBLE PRECISION
+);
+
+
+
+ CREATE TABLE Prescription
+(
+PrescriptionID INT PRIMARY KEY, 
+MedID INT NOT NULL
+REFERENCES MedicationDetails(MedID),
+PatEncID INT NOT NULL
+REFERENCES PatientEncounter(PatEncID),
+HealthCareProviderID INT NOT NULL
+REFERENCES HealthCareProvider(HealthCareProviderID),
+PrescStartDate DATETIME,
+PrescEndDate DATETIME,
+PrescDose FLOAT,
+PrescQty FLOAT
+);
+
+
+
+CREATE TABLE Billing
+(
+BillingID INT PRIMARY KEY, 
+PatEncID INT NOT NULL
+REFERENCES PatientEncounter(PatEncID),
+OrderTotal DOUBLE PRECISION,
+PaymentStatus VARCHAR(45),
+ClaimSanctionAmt DOUBLE PRECISION
+);
