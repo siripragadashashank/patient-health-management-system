fdd1156dd9130465a4653fe7ae85496d4742b0e9
mayannk27 <kumaar.m@northeastern.edu>
2022-04-12 21:26:37 -0400

    organisation and db insert queries

---
+++ Queries/db-insert-mayannk.sql
@@ -0,0 +1,30 @@
+USE PHMS
+
+insert into DiagnosisDetails(DxCode, DxName) values (7, 'Congestive heart failure (CHF)');
+
+select * from DiagnosisDetails
+
+insert into DiagnosisDetails(DxCode, DxName) values (8, 'Acute myocardial infarction');
+insert into DiagnosisDetails(DxCode, DxName) values (9, 'Cardiac dysrhythmia');
+insert into DiagnosisDetails(DxCode, DxName) values (12, 'Lumbago');
+insert into DiagnosisDetails(DxCode, DxName) values (15, 'Chronic obstructive pulmonary disease (COPD)');
+insert into DiagnosisDetails(DxCode, DxName) values (19, 'Atrial fibrillation');
+insert into DiagnosisDetails(DxCode, DxName) values (25, 'Diabetes Type I');
+insert into DiagnosisDetails(DxCode, DxName) values (26, 'Diabetes Type II');
+insert into DiagnosisDetails(DxCode, DxName) values (27, 'Urinary tract infection (UTI)');
+insert into DiagnosisDetails(DxCode, DxName) values (34, 'Abdominal infection');
+insert into DiagnosisDetails(DxCode, DxName) values (35, 'Osteoarthritis');
+insert into DiagnosisDetails(DxCode, DxName) values (42, 'Jaundice');
+insert into DiagnosisDetails(DxCode, DxName) values (57, 'Hypertension');
+insert into DiagnosisDetails(DxCode, DxName) values (60, 'Surgical Complication');
+insert into DiagnosisDetails(DxCode, DxName) values (62, 'Heart failure');
+insert into DiagnosisDetails(DxCode, DxName) values (64, 'Acute bronchitis');
+insert into DiagnosisDetails(DxCode, DxName) values (67, 'Pneumonia');
+insert into DiagnosisDetails(DxCode, DxName) values (68, 'COVID-19');
+insert into DiagnosisDetails(DxCode, DxName) values (71, 'Basal cell carcinoma of skin');
+insert into DiagnosisDetails(DxCode, DxName) values (72, 'Carcinoma in stomach');
+insert into DiagnosisDetails(DxCode, DxName) values (73, 'Carcinoma in eye');
+insert into DiagnosisDetails(DxCode, DxName) values (80, 'Cerebral infraction');
+insert into DiagnosisDetails(DxCode, DxName) values (84, 'Clostridium difficile (C.Diff)');
+insert into DiagnosisDetails(DxCode, DxName) values (98, 'Sepsis');
+insert into DiagnosisDetails(DxCode, DxName) values (99, 'Severe sepsis');
