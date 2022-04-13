52a37cf390f1beab4acc1f3958f1e9a222bf6ce9
Shashank Siripragada <siripragada.s@northeastern.edu>
2022-04-12 23:05:31 -0400

    Update db-insert-shashank.sql

--- Queries/db-insert-shashank.sql
+++ Queries/db-insert-shashank.sql
@@ -1,4 +1,37 @@
 ---------------------------------------------------Insert---------------------------------------------------------------------
+
+
+
 --------------------------------------Patient--------------------------------------------------------------------------------
-insert into Patient(PatID, FirstName, LastName, DoB, Street, City, State, ZipCode, PhoneNo, EmailAddress) 
-values (101, 'Oscar' ,'Yang','1962-06-10', '2210 Elmwood Avenue', 'Mesa', 'Arizona',85201,4809625179, 'oscar.yang@gmail.com');
+
+insert into Patient(PatID, FirstName, LastName, DoB, Street, City, State, ZipCode, PhoneNo, EmailAddress) values (101, 'Oscar' ,'Yang','1962-06-10', '2210 Elmwood Avenue', 'Mesa', 'Arizona',85201,4809625179, 'oscar.yang@gmail.com');
+insert into Patient(PatID, FirstName, LastName, DoB, Street, City, State, ZipCode, PhoneNo, EmailAddress) values (102, 'Richard' ,'Rice','1962-10-14', '4013 Southern Avenue', 'Eureka', 'Missouri',63025,6369382484, 'richard.rice@gmail.com');
+insert into Patient(PatID, FirstName, LastName, DoB, Street, City, State, ZipCode, PhoneNo, EmailAddress) values (103, 'Joshua' ,'Dean','1963-04-13', '4877 Lynch Street', 'Oakland', 'California',94612,9252096032, 'joshua.dean@gmail.com');
+insert into Patient(PatID, FirstName, LastName, DoB, Street, City, State, ZipCode, PhoneNo, EmailAddress) values (104, 'William' ,'Ruiz','1963-12-22', '1356 Worthington Drive', 'Red Oak', 'Texas',75154,9725767414, 'william.ruiz@gmail.com');
+insert into Patient(PatID, FirstName, LastName, DoB, Street, City, State, ZipCode, PhoneNo, EmailAddress) values (105, 'Joseph' ,'Kent','1964-06-16', '157 Lynn Ogden Lane', 'Beaumont', 'Texas',77705,4098407436, 'joseph.kent@gmail.com');
+insert into Patient(PatID, FirstName, LastName, DoB, Street, City, State, ZipCode, PhoneNo, EmailAddress) values (106, 'Joshua' ,'Alexander','1967-07-16', '2132 Haul Road', 'Lansing', 'Ohio',43934,6509955757, 'joshua.alexander@gmail.com');
+insert into Patient(PatID, FirstName, LastName, DoB, Street, City, State, ZipCode, PhoneNo, EmailAddress) values (107, 'Robert' ,'Richards','1968-12-29', '3600 Holt Street', 'West Palm Beach', 'Florida',33410,5612612075, 'robert.richards@gmail.com');
+insert into Patient(PatID, FirstName, LastName, DoB, Street, City, State, ZipCode, PhoneNo, EmailAddress) values (108, 'David' ,'Mejia','1970-05-14', '3444 Libby Street', 'Beverly Hills', 'California',90210,3102767127, 'david.mejia@gmail.com');
+insert into Patient(PatID, FirstName, LastName, DoB, Street, City, State, ZipCode, PhoneNo, EmailAddress) values (109, 'Jeffrey' ,'Sutton','1973-12-03', '1468 Midway Road', 'Ratcliff', 'Arkansas',72951,4796353319, 'jeffrey.sutton@gmail.com');
+insert into Patient(PatID, FirstName, LastName, DoB, Street, City, State, ZipCode, PhoneNo, EmailAddress) values (110, 'Ronald' ,'Mitchell','1974-02-06', '1962 Oakwood Circle', 'Riverside', 'California',92501,9493181359, 'ronald.mitchell@gmail.com');
+insert into Patient(PatID, FirstName, LastName, DoB, Street, City, State, ZipCode, PhoneNo, EmailAddress) values (111, 'Micheal' ,'Hall','1974-06-10', '13 Desert Broom Court', 'Jersey City', 'New Jersey',73040,2017183614, 'micheal.hall@gmail.com');
+insert into Patient(PatID, FirstName, LastName, DoB, Street, City, State, ZipCode, PhoneNo, EmailAddress) values (112, 'Joshua' ,'Noble','1976-02-20', '2688 Pritchard Court', 'Owatonna', 'Minnesota',55060,5074087953, 'joshua.noble@gmail.com');
+insert into Patient(PatID, FirstName, LastName, DoB, Street, City, State, ZipCode, PhoneNo, EmailAddress) values (113, 'Brett' ,'Walker','1978-01-29', '3881 Heron Way', 'Portland', 'Oregon',97205,5038304159, 'brett.walker@gmail.com');
+insert into Patient(PatID, FirstName, LastName, DoB, Street, City, State, ZipCode, PhoneNo, EmailAddress) values (114, 'Mark' ,'Swanson','1979-10-18', '2166 Tibbs Avenue', 'Helena', 'Montana',59601,4067818712, 'mark.swanson@gmail.com');
+insert into Patient(PatID, FirstName, LastName, DoB, Street, City, State, ZipCode, PhoneNo, EmailAddress) values (115, 'John' ,'Thompson','1981-09-27', '4962 Pearl Street', 'Sacramento', 'California',95814,9163164566, 'john.thompson@gmail.com');
+insert into Patient(PatID, FirstName, LastName, DoB, Street, City, State, ZipCode, PhoneNo, EmailAddress) values (116, 'Lauren' ,'Kim','1983-07-05', '1905 Simpson Avenue', 'Akron', 'Pennsylvania',17501,7178590339, 'lauren.kim@gmail.com');
+insert into Patient(PatID, FirstName, LastName, DoB, Street, City, State, ZipCode, PhoneNo, EmailAddress) values (117, 'Alisha' ,'Smith','1984-06-19', '2899 Buffalo Creek Road', 'Franklin', 'Tennessee',37064,6157940426, 'alisha.smith@gmail.com');
+insert into Patient(PatID, FirstName, LastName, DoB, Street, City, State, ZipCode, PhoneNo, EmailAddress) values (118, 'Laura' ,'Rodgers','1986-10-16', '4305 Godfrey Street', 'Hillsboro', 'Oregon',97123,5036286733, 'laura.rodgers@gmail.com');
+insert into Patient(PatID, FirstName, LastName, DoB, Street, City, State, ZipCode, PhoneNo, EmailAddress) values (119, 'Margaret' ,'Powers','1991-01-02', '4935 Catherine Drive', 'East Grand Forks', 'North Dakota',56721,7013179341, 'margaret.powers@gmail.com');
+insert into Patient(PatID, FirstName, LastName, DoB, Street, City, State, ZipCode, PhoneNo, EmailAddress) values (120, 'Amy' ,'Phelps','1991-01-15', '364 Bingamon Branch Road', 'Sullivan', 'Wisconsin',53178,8472445848, 'amy.phelps@gmail.com');
+insert into Patient(PatID, FirstName, LastName, DoB, Street, City, State, ZipCode, PhoneNo, EmailAddress) values (121, 'Veronica' ,'Dawson','1991-12-10', '921 Marie Street', 'Annapolis', 'Maryland',21401,4108587688, 'veronica.dawson@gmail.com');
+insert into Patient(PatID, FirstName, LastName, DoB, Street, City, State, ZipCode, PhoneNo, EmailAddress) values (122, 'Amanda' ,'Lucero','1992-12-06', '2951 Olen Thomas Drive', 'Wichita Falls', 'Texas',76301,9405576186, 'amanda.lucero@gmail.com');
+insert into Patient(PatID, FirstName, LastName, DoB, Street, City, State, ZipCode, PhoneNo, EmailAddress) values (123, 'Tonya' ,'Miller','1994-07-03', '1311 Rafe Lane', 'Greenwood', 'Mississippi',38930,6627142782, 'tonya.miller@gmail.com');
+insert into Patient(PatID, FirstName, LastName, DoB, Street, City, State, ZipCode, PhoneNo, EmailAddress) values (124, 'Karen' ,'Maynard','1999-02-18', '599 Lilac Lane', 'Darien', 'Georgia',31305,9124377159, 'karen.maynard@gmail.com');
+insert into Patient(PatID, FirstName, LastName, DoB, Street, City, State, ZipCode, PhoneNo, EmailAddress) values (125, 'Alyssa' ,'Miranda','1999-04-01', '3128 Tori Lane', 'Greenview', 'Illinois',62642,8016455363, 'alyssa.miranda@gmail.com');
+insert into Patient(PatID, FirstName, LastName, DoB, Street, City, State, ZipCode, PhoneNo, EmailAddress) values (126, 'Jennifer' ,'Harrington','1964-03-08', '41 Spadafore Drive', 'State College', 'Pennsylvania',16801,8147150156, 'jennifer.harrington@gmail.com');
+insert into Patient(PatID, FirstName, LastName, DoB, Street, City, State, ZipCode, PhoneNo, EmailAddress) values (127, 'Stacy' ,'White','1968-10-26', '3915 Rhapsody Street', 'Inverness', 'Florida',32650,3523446389, 'stacy.white@gmail.com');
+insert into Patient(PatID, FirstName, LastName, DoB, Street, City, State, ZipCode, PhoneNo, EmailAddress) values (128, 'Amanda' ,'Le','1984-06-17', '1271 Summit Park Avenue', 'Southfield', 'Michigan',48034,2489482017, 'amanda.le@gmail.com');
+insert into Patient(PatID, FirstName, LastName, DoB, Street, City, State, ZipCode, PhoneNo, EmailAddress) values (129, 'Tracy' ,'Peterson','1988-02-08', '4976 McKinley Avenue', 'Denver', 'Colorado',80202,3038072071, 'tracy.peterson@gmail.com');
+insert into Patient(PatID, FirstName, LastName, DoB, Street, City, State, ZipCode, PhoneNo, EmailAddress) values (130, 'Amy' ,'Martin','1992-11-16', '2078 Red Dog Road', 'Charlotte', 'North Carolina',28202,7043395859, 'amy.martin@gmail.com');
+
