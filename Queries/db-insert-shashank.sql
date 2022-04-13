e632d8915fa4c888d6ea7dc182e6423d9d82aecb
Shashank Siripragada <siripragada.s@northeastern.edu>
2022-04-12 21:27:44 -0400

    Create db-insert-shashank.sql

---
+++ Queries/db-insert-shashank.sql
@@ -0,0 +1,4 @@
+---------------------------------------------------Insert---------------------------------------------------------------------
+--------------------------------------Patient--------------------------------------------------------------------------------
+insert into Patient(PatID, FirstName, LastName, DoB, Street, City, State, ZipCode, PhoneNo, EmailAddress) 
+values (101, 'Oscar' ,'Yang','1962-06-10', '2210 Elmwood Avenue', 'Mesa', 'Arizona',85201,4809625179, 'oscar.yang@gmail.com');
