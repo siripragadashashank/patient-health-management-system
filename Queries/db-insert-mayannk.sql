c461e8545b36cc557e8b882bc3c80d28ba9f6d5b
mayannk27 <kumaar.m@northeastern.edu>
2022-04-13 17:56:32 -0400

    epoc table

--- Queries/db-insert-mayannk.sql
+++ Queries/db-insert-mayannk.sql
@@ -71,4 +71,36 @@ insert into LabResultDetails(TestID, TestName, Price) values (197, 'Bone Fractur
 
 select * from LabResultDetails
 
---------------------------------------------------
+-------------------------EPOC-------------------------
+
+insert into EPOC (EPOCID,PatID, EPOCFirstName, EPOCLastName, EPOCPhoneNo) values (1,101, 'Anthony' ,'Mendoza',3607027103);
+insert into EPOC (EPOCID,PatID, EPOCFirstName, EPOCLastName, EPOCPhoneNo) values (2,102, 'Colleen' ,'Reed',3346093248);
+insert into EPOC (EPOCID,PatID, EPOCFirstName, EPOCLastName, EPOCPhoneNo) values (3,103, 'Bobbie' ,'Harper',6463538347);
+insert into EPOC (EPOCID,PatID, EPOCFirstName, EPOCLastName, EPOCPhoneNo) values (4,104, 'Bernard' ,'Mccormick',4127741666);
+insert into EPOC (EPOCID,PatID, EPOCFirstName, EPOCLastName, EPOCPhoneNo) values (5,105, 'Alonzo' ,'Allen',4805511804);
+insert into EPOC (EPOCID,PatID, EPOCFirstName, EPOCLastName, EPOCPhoneNo) values (6,106, 'Emilio' ,'Klein',2675371407);
+insert into EPOC (EPOCID,PatID, EPOCFirstName, EPOCLastName, EPOCPhoneNo) values (7,107, 'Toni' ,'Collins',5592178932);
+insert into EPOC (EPOCID,PatID, EPOCFirstName, EPOCLastName, EPOCPhoneNo) values (8,108, 'Arlene' ,'Bridges',7725386959);
+insert into EPOC (EPOCID,PatID, EPOCFirstName, EPOCLastName, EPOCPhoneNo) values (9,109, 'Rufus' ,'Russell',3473391104);
+insert into EPOC (EPOCID,PatID, EPOCFirstName, EPOCLastName, EPOCPhoneNo) values (10,110, 'Salvador' ,'Barnett',3402205144);
+insert into EPOC (EPOCID,PatID, EPOCFirstName, EPOCLastName, EPOCPhoneNo) values (11,111, 'Tracy' ,'Mullins',2814701730);
+insert into EPOC (EPOCID,PatID, EPOCFirstName, EPOCLastName, EPOCPhoneNo) values (12,112, 'Glen' ,'Ruiz',7753539431);
+insert into EPOC (EPOCID,PatID, EPOCFirstName, EPOCLastName, EPOCPhoneNo) values (13,113, 'Conrad' ,'Patrick',6083474141);
+insert into EPOC (EPOCID,PatID, EPOCFirstName, EPOCLastName, EPOCPhoneNo) values (14,114, 'Georgia' ,'Hunter',4195306136);
+insert into EPOC (EPOCID,PatID, EPOCFirstName, EPOCLastName, EPOCPhoneNo) values (15,115, 'Brendan' ,'Daniel',2139638469);
+insert into EPOC (EPOCID,PatID, EPOCFirstName, EPOCLastName, EPOCPhoneNo) values (16,116, 'Tricia' ,'Vaughn',2066024801);
+insert into EPOC (EPOCID,PatID, EPOCFirstName, EPOCLastName, EPOCPhoneNo) values (17,117, 'Christian' ,'Armstrong',4432104071);
+insert into EPOC (EPOCID,PatID, EPOCFirstName, EPOCLastName, EPOCPhoneNo) values (18,118, 'Casey' ,'Huff',5673514078);
+insert into EPOC (EPOCID,PatID, EPOCFirstName, EPOCLastName, EPOCPhoneNo) values (19,119, 'Luz' ,'Moss',2603571958);
+insert into EPOC (EPOCID,PatID, EPOCFirstName, EPOCLastName, EPOCPhoneNo) values (20,120, 'Alexis' ,'Rivera',4082447244);
+insert into EPOC (EPOCID,PatID, EPOCFirstName, EPOCLastName, EPOCPhoneNo) values (21,121, 'June' ,'Herrera',4108587688);
+insert into EPOC (EPOCID,PatID, EPOCFirstName, EPOCLastName, EPOCPhoneNo) values (22,122, 'Marilyn' ,'Reyes',9405576186);
+insert into EPOC (EPOCID,PatID, EPOCFirstName, EPOCLastName, EPOCPhoneNo) values (23,123, 'Penny' ,'Harrison',6627142782);
+insert into EPOC (EPOCID,PatID, EPOCFirstName, EPOCLastName, EPOCPhoneNo) values (24,124, 'Kyle' ,'Sims',9124377159);
+insert into EPOC (EPOCID,PatID, EPOCFirstName, EPOCLastName, EPOCPhoneNo) values (25,125, 'Elvira' ,'Howard',8016455363);
+insert into EPOC (EPOCID,PatID, EPOCFirstName, EPOCLastName, EPOCPhoneNo) values (26,126, 'Jeannie' ,'Morrison',8147150156);
+insert into EPOC (EPOCID,PatID, EPOCFirstName, EPOCLastName, EPOCPhoneNo) values (27,127, 'Jo' ,'Boyd',3523446389);
+insert into EPOC (EPOCID,PatID, EPOCFirstName, EPOCLastName, EPOCPhoneNo) values (28,128, 'Barbara' ,'Walsh',2489482017);
+insert into EPOC (EPOCID,PatID, EPOCFirstName, EPOCLastName, EPOCPhoneNo) values (29,129, 'Tony' ,'Silva',3038072071);
+insert into EPOC (EPOCID,PatID, EPOCFirstName, EPOCLastName, EPOCPhoneNo) values (30,130, 'Jose' ,'Richards',7043395859);
+select * from EPOC
