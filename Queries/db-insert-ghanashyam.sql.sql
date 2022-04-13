e7b1b569c15678e9a3db95f9b72a9fb23f5f6ea1
Ghanashyam0075 <103465065+Ghanashyam0075@users.noreply.github.com>
2022-04-12 22:59:15 -0400

    Create db-insert-ghanashyam.sql.sql

---
+++ Queries/db-insert-ghanashyam.sql.sql
@@ -0,0 +1,27 @@
+Use Phms
+----- SymptomDetails
+insert into SymptomDetails(SymCode, SymName) values (10, 'Abdominal pain');
+insert into SymptomDetails(SymCode, SymName) values (11, 'Chest Pain');
+insert into SymptomDetails(SymCode, SymName) values (12, 'Stomach Flu');
+insert into SymptomDetails(SymCode, SymName) values (13, 'Weight loss');
+insert into SymptomDetails(SymCode, SymName) values (14, 'Fever');
+insert into SymptomDetails(SymCode, SymName) values (15, 'Fatigue');
+insert into SymptomDetails(SymCode, SymName) values (16, 'Nausea');
+insert into SymptomDetails(SymCode, SymName) values (17, 'Jaundice');
+insert into SymptomDetails(SymCode, SymName) values (18, 'Anxiety');
+insert into SymptomDetails(SymCode, SymName) values (19, 'Bad cough');
+insert into SymptomDetails(SymCode, SymName) values (20, 'Vomiting');
+insert into SymptomDetails(SymCode, SymName) values (21, 'Internal Bleeding');
+insert into SymptomDetails(SymCode, SymName) values (22, 'Skin Infection');
+insert into SymptomDetails(SymCode, SymName) values (23, 'Anal Pain');
+insert into SymptomDetails(SymCode, SymName) values (24, 'Back Pain');
+insert into SymptomDetails(SymCode, SymName) values (25, 'Blood Clots');
+insert into SymptomDetails(SymCode, SymName) values (26, 'Epiphora');
+insert into SymptomDetails(SymCode, SymName) values (27, 'Painful Urination');
+insert into SymptomDetails(SymCode, SymName) values (28, 'Hyperhidrosis');
+insert into SymptomDetails(SymCode, SymName) values (29, 'Blurry Vision');
+insert into SymptomDetails(SymCode, SymName) values (30, 'Joint Pain');
+insert into SymptomDetails(SymCode, SymName) values (31, 'Shortness in breath');
+insert into SymptomDetails(SymCode, SymName) values (32, 'Headache');
+insert into SymptomDetails(SymCode, SymName) values (33, 'Disorientation');
+
